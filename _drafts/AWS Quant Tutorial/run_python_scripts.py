e75b5372e4bb6f080e28f0086d1af8e973e3866f
bshabash <bshabash007@outlook.com>
2020-01-26 14:19:13 -0800

    checked command line arguments for AWS requests

--- _drafts/AWS Quant Tutorial/run_python_scripts.py
+++ _drafts/AWS Quant Tutorial/run_python_scripts.py
@@ -1,5 +1,11 @@
 import os
+import sys
+import time
+
+
 
 for STOCK_ID in ['AAPL', 'MSFT']:
-    cmd = 'python3 /home/hadoop/obtain_data.py '+STOCK_ID+' --output_directory s3://boris-data-files'
+    cmd = 'python3 /home/hadoop/obtain_data.py '+STOCK_ID+' --output_directory '+sys.argv[1]
+    #cmd = 'python3 /home/hadoop/obtain_data.py '+STOCK_ID+' --output_directory s3://boris-data-files'
     os.system(cmd)
+    time.sleep(20)
